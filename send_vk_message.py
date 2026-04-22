1c990ebd220adb64b40844f8445ee6a8427d9608
Aferokript <alberto.cardinale@bk.ru>
2026-04-22 21:53:02 +0300

    Упростил код и добавил функцию отправки фото

--- send_vk_message.py
+++ send_vk_message.py
@@ -2,40 +2,51 @@ import requests
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 
+VK_URL = "https://api.vk.com/method/wall.post"
 VK_API_VERSION = 5.199
+VK_TOKEN = os.environ['VK_TOKEN']
+OWNER_ID = os.environ['OWNER_ID']
+
 
 def send_vk_message(message, from_group=1):
-    vk_url = "https://api.vk.com/method/wall.post"
-    vk_token = os.environ['VK_TOKEN']
-    owner_id = os.environ['OWNER_ID']
-
     params = {
-        'access_token': vk_token,
+        'access_token': VK_TOKEN,
         'v': VK_API_VERSION,
-        'owner_id': owner_id,
+        'owner_id': OWNER_ID,
         'message': message,
         'from_group': from_group,
     }
 
     response = requests.get(vk_url, params=params)
     response.raise_for_status()
-
     message_data = response.json()
     post_id = message_data['response']['post_id']
     return post_id
 
 
-def delete_vk_message(post_id):
-    vk_url = "https://api.vk.com/method/wall.delete"
-    vk_token = os.environ['VK_TOKEN']
-    owner_id = os.environ['OWNER_ID']
-
+def send_vk_photo(attachments, from_group=1):
     params = {
-        'access_token': vk_token,
+        'access_token': VK_TOKEN,
         'v': VK_API_VERSION,
-        'owner_id': owner_id,
+        'owner_id': OWNER_ID,
+        'attachments': '2026-04-22_17-49-28.png'
+    }
+    response = requests.get(vk_url, params=params)
+    response.raise_for_status()
+
+    photo_data = response.json()
+    post_id = photo_data[0]['id']
+    return post_id
+
+
+def delete_vk_message(post_id):
+    params = {
+        'access_token': VK_TOKEN,
+        'v': VK_API_VERSION,
+        'owner_id': OWNER_ID,
         'post_id': post_id,
 
     }
@@ -43,3 +54,6 @@ def delete_vk_message(post_id):
     response = requests.get(vk_url, params=params)
     response.raise_for_status()
     return f'Пост {post_id} успешно удален'
+
+
+send_vk_message('Привет')

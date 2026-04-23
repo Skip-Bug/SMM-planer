da2222e154afba45422cb632d5026822a8d94aa7
Aferokript <alberto.cardinale@bk.ru>
2026-04-23 10:59:44 +0300

    Добавил возврат id сообщения, снятие галочки

--- heard.py
+++ heard.py
@@ -10,31 +10,30 @@ def read_sheet():
     3. Отправить данные из таблицы
     4. Вернуть id поста и статус '''
     sheet_url = 'https://docs.google.com/spreadsheets/d/1MebKepXP5of1ZUVzMkGDDWwhok2DQq3ncYngKCeLDJI/edit?usp=sharing'
+    sheet = gc.open_by_url(sheet_url)
     while True:
-        sheet = gc.open_by_url(sheet_url)
         sheet_data = sheet.get_all_values()
         message = sheet.col_values(2)
         vk_checkbox = sheet.col_values(4)
         telegram_checkbox = sheet.col_values(5)
         classniki_checkbox = sheet.col_values(6)
 
-        for row in sheet_data:
+        for i, row in enumerate(sheet_data[1:], start=2):
             vk = row[4]
             tg = row[5]
             classniki = row[6]
 
-            if vk == True:
-                print(f'Отправка {message} в вк ')
-            if tg == True:
+            if vk == 'TRUE':
+                post_id = send_vk_message.send_vk_message(message)
+                sheet.update(f'H{i}', post_id)
+                sheet.update(f'E{i}', 'FALSE')
+            if tg == 'TRUE':
                 print(f'Отправка {message} в тг')
-            if classniki == True:
+            if classniki == 'TRUE':
                 print(f'Отправка {message} в одноклассники')
 
 
-
-
-
-
+        time.sleep(60)
 
 
 

93c4e0baacc0b38680178fc53cba776be507ab2c
Leonid Stoyko <gribojor@gmail.com>
2026-04-25 16:19:37 +0500

    Update heard.py

--- heard.py
+++ heard.py
@@ -6,6 +6,7 @@ from content_loader import load_content, load_image
 import tg_poster
 import requests
 
+
 def read_sheet():
     '''Функция должна:
     1. открыть таблицу
@@ -27,41 +28,42 @@ def read_sheet():
     while True:
         sheet_data = sheet.get_all_values()
         for i, row in enumerate(sheet_data[1:], start=2):
-           vk = row[3]
-           ok = row[4]
-           tg = row[5]
-           id = row[11]
-           sourse = row[0]
-           picture = row[1]
-           message = load_content(sourse)
+            vk = row[3]
+            ok = row[4]
+            tg = row[5]
+            id = row[11]
+            sourse = row[0]
+            picture = row[1]
+            message = load_content(sourse)
 
-           print(f"Строка {i}: vk={vk}, messages={message}, picture={picture}")
+            print(
+                f"Строка {i}: vk={vk}, messages={message}, picture={picture}")
 
-           try:
-               if vk == 'TRUE':
-                   post_id = send_vk_message.send_vk_message(message)
-                   sheet.update(f'L{i}', [[post_id]])
-                   sheet.update(f'G{i}', [[status[1]]])
-           except requests.exceptions.RequestException as e:
-               sheet.update(f'G{i}', [[status[3]]])
-               
-           try:
-               if tg == 'TRUE':
-                   post_id = send_text()
-                   sheet.update(f'P{i}', [[post_id]])
-                   sheet.update(f'G{i}', [[status[1]]])
-           except requests.exceptions.RequestException as e:
-               sheet.update(f'I{i}', [[status[3]]])
+            try:
+                if vk == 'TRUE':
+                    post_id = send_vk_message.send_vk_message(message)
+                    sheet.update(f'L{i}', [[post_id]])
+                    sheet.update(f'G{i}', [[status[1]]])
+            except requests.exceptions.RequestException as e:
+                sheet.update(f'G{i}', [[status[3]]])
 
-           try:
-               if ok == 'TRUE':
-                   post_id = post_to_ok.post_to_ok(message)
-                   sheet.update(f'N{i}', [[post_id]])
-                   sheet.update(f'H{i}', [[status[1]]])
-           except requests.exceptions.RequestException as e:
-               sheet.update(f'H{i}', [[status[3]]])
+            try:
+                if tg == 'TRUE':
+                    post_id = send_text()
+                    sheet.update(f'P{i}', [[post_id]])
+                    sheet.update(f'G{i}', [[status[1]]])
+            except requests.exceptions.RequestException as e:
+                sheet.update(f'I{i}', [[status[3]]])
 
-           time.sleep(60)
+            try:
+                if ok == 'TRUE':
+                    post_id = post_to_ok.post_to_ok(message)
+                    sheet.update(f'N{i}', [[post_id]])
+                    sheet.update(f'H{i}', [[status[1]]])
+            except requests.exceptions.RequestException as e:
+                sheet.update(f'H{i}', [[status[3]]])
+
+            time.sleep(60)
 
 
 read_sheet()
